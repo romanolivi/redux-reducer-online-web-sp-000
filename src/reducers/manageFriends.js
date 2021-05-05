b5a612d0353b698efbaebe6f3cd1fc4340136da7
romanolivi <romano_livi@outlook.com>
2021-05-04 23:11:04 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -4,27 +4,17 @@ export function manageFriends(state, action){
     switch (action.type) {
         case 'ADD_FRIEND': 
             return (
-                {...state, 
-                    friends: [
-                        ...state.friends,
-                        action.friend
-                    ]
-                }
+                {...state, friends: [...state.friends, action.friend] }
             )
         
         case "REMOVE_FRIEND":
             let removeFriend = state.friends.findIndex(friend => friend.id === action.id);
             return (
-                {...state,
-                    friends: [
-                        ...state.friends.slice(0, removeFriend),
-                        ...state.friends.slice(removeFriend + 1)
-                    ]
-                }
+                {...state, friends: [...state.friends.slice(0, removeFriend), ...state.friends.slice(removeFriend + 1)] }
             )
 
         default: 
             return state;
-            
+
     }
 }
