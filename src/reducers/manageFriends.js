e8d2e052a38d18faf3ddcbb95dc8d118750c96c8
romanolivi <romano_livi@outlook.com>
2021-05-04 22:34:19 -0400

    commit tree 67e2a294313d5aa47dcee364404bacf6621459f4

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,30 @@
+let state = {friends: []}
+
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND': 
+            return (
+                {...state, 
+                    friends: [
+                        ...state.friends,
+                        action.friend
+                    ]
+                }
+            )
+        
+        case "REMOVE_FRIEND":
+            let removeFriend = state.friends.findIndex(friend => friend.id === action.id);
+            return (
+                {...state,
+                    friends: [
+                        ...state.friends.slice(0, removeFriend),
+                        ...state.friends.slice(removeFriend + 1)
+                    ]
+                }
+            )
+
+        default: 
+            return state;
+            
+    }
 }
